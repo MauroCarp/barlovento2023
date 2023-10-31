447a17ddab70bd606001ccb42716ee60edd75f04
Mauro <maurogt.dd@gmail.com>
2023-10-31 16:23:21 -0300

    acceso nuevo modulo trazabilidad

---
+++ vistas/modulos/trazabilidad/index.php
@@ -0,0 +1,37 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Trazabilidad
+      
+      <small>Panel de Control</small>
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Trazabilidad</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+    <h1>Trazabilidad</h1>
+    <div class="row">
+
+          <div class="col-lg-12" style="padding: 50px">
+            <br>
+            <img src="vistas/img/plantilla/logo-barlovento.png" style="width:100%;">
+          
+          </div>
+
+    </div>
+
+  </section>
+ 
+</div>
